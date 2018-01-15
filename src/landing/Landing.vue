8325ff0fc4b85135f29828b0a95d36622dcd3406
Obinna Ngini <obinnangini@gmail.com>
2018-01-15 10:42:12 -0500

    Update HTML structure

--- src/landing/Landing.vue
+++ src/landing/Landing.vue
@@ -2,13 +2,13 @@
   <div class="jumbotron" style="margin: 3rem">
     <div style="text-align: center">
       <h1 class="display-3">Obi Bootstrap Style Guide</h1>
-      <p class="lead">
+      <section class="lead">
         This site showcases select Bootstrap components, and accompanying Vue.js code samples.
-        All styled with the <a href="https://github.com/obinnangini/obi-bootstrap">obi-bootstrap</a> stylesheet
+        All styled with the <a href="https://github.com/obinnangini/obi-bootstrap">obi-bootstrap</a> stylesheet.
         <div class="mx-auto">
           <router-link class="btn btn-primary" :to="{name: 'Components'}">Components</router-link>
         </div>
-      </p>
+      </section>
     </div>
   </div>
 </template>
